d4be2eaec44563cd5f98544e5a515713b5a0fc0b
Goutam Mohanty <30702929+Goutam997@users.noreply.github.com>
2022-01-30 12:41:04 +0000

    added the active/completed views

--- src/App.jsx
+++ src/App.jsx
@@ -2,84 +2,96 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-let [task, setTask] = useState("");
-let [todo_items, setTodo] = useState(
+  let [task, setTask] = useState("");
+  let [todo_items, setTodo] = useState(
     [{
       content: "Create theme",
       status: false
     },
     {
-      content:"Work on wordpress",
+      content: "Work on wordpress",
       status: false
     },
     {
-      content:"Organize office main department",
+      content: "Organize office main department",
       status: false
     },
     {
-      content:"Error solve in HTML template",
+      content: "Error solve in HTML template",
       status: false
     }]
-);
+  );
 
-let changeStatus = (obj) =>{
-  let update = [...todo_items];
-  let item = obj;
-  item.status = !item.status;
-  // console.log(obj);
-  update.splice(update.indexOf(obj),1, item)
-  setTodo(update);
-};
+  // const [currentTab, setTab] = useState("all");
 
-let add = () => {
-  if(task){
-    let newArr = [...todo_items];
-    newArr.push({
-      content: task,
-      status: false
-    })
-    setTask("");
-    setTodo(newArr);
-    console.log(newArr);
-  }
-};
+  const [displayItems, setItems] = useState(todo_items);
+
+  let changeStatus = (obj) => {
+    let update = [...todo_items];
+    let item = obj;
+    item.status = !item.status;
+    // console.log(obj);
+    update.splice(update.indexOf(obj), 1, item)
+    setTodo(update);
+  };
+
+  let add = () => {
+    if (task) {
+      let newArr = [...todo_items];
+      newArr.push({
+        content: task,
+        status: false
+      })
+      setTask("");
+      setTodo(newArr);
+      setItems(newArr);
+      console.log(newArr);
+    }
+  };
 
   return <>
     <div className="container">
-    <div className="row">
+      <div className="row">
         <div className="col-md-12">
-            <div className="card card-white">
-                <div className="card-body">
-                    <form action="javascript:void(0);">
-                        <input type="text" className="form-control add-task" placeholder="New Task..." onChange={(e) => {setTask(e.target.value)
-                        console.log(task);
-                        }} onKeyDown = {
-                          (e) => {
-                          if(e.key==="Enter")
-                            add()
-                          }
-                          }/>
-                    </form>
-                    <ul className="nav nav-pills todo-nav">
-                        <li role="presentation" className="nav-item all-task active"><a href="#" className="nav-link">All</a></li>
-                        <li role="presentation" className="nav-item active-task"><a href="#" className="nav-link">Active</a></li>
-                        <li role="presentation" className="nav-item completed-task"><a href="#" className="nav-link">Completed</a></li>
-                    </ul>
-                    <div className="todo-list">
-                      {  todo_items.map((e,index) => {
-                          return <div className="todo-item" key={index}>
-                            <div className="checker"><span className=""><input type="checkbox" onChange= {() => changeStatus(e)}/></span></div>
-                            {e.status?<span><s>{e.content}</s></span>:<span>{e.content}</span>}
-                            <a href="javascript:void(0);" className="float-right remove-todo-item"><i className="icon-close"></i></a>
-                          </div>
-                        })
-                        }
-                    </div>
-                </div>
+          <div className="card card-white">
+            <div className="card-body">
+              <form>
+                <input type="text" className="form-control add-task" placeholder="New Task..." onChange={(e) => {
+                  setTask(e.target.value)
+                  console.log(task);
+                }} onKeyDown={
+                  (e) => {
+                    if (e.key === "Enter")
+                      add()
+                  }
+                } />
+              </form>
+              <ul className="nav nav-pills todo-nav">
+                <li role="presentation" onClick={() => {
+                  setItems(todo_items);
+                }} className="nav-item all-task active"><a href="#" className="nav-link">All</a></li>
+                <li role="presentation" onClick={() => {
+                  setItems(todo_items.filter(({ status }) => !status));
+                }} className="nav-item active-task"><a href="#" className="nav-link">Active</a></li>
+                <li role="presentation" onClick={() => {
+                  setItems(todo_items.filter(({ status }) => status));
+                }} className="nav-item completed-task"><a href="#" className="nav-link">Completed</a></li>
+              </ul>
+              <div className="todo-list">
+                {displayItems.map((e, index) => {
+                  return <div className="todo-item" key={e.content}>
+                    <div className="checker"><span className=""><input type="checkbox" onChange={() => changeStatus(e)} checked={e.status} /></span></div>
+                    {e.status ? <span><s>{e.content}</s></span> : <span>{e.content}</span>}
+                    <a href="javascript:void(0);" className="float-right remove-todo-item"><i className="icon-close"></i></a>
+                  </div>
+                })
+                }
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  </div>  
   </>;
 }
 
